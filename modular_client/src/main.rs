99710e26ecf1b15f001af97c85b0087a70974dad
Dan Lewis <daniel.lewis.software.engineer@gmail.com>
2020-12-21 00:29:11 -0600

    femtovg

--- modular_client/src/main.rs
+++ modular_client/src/main.rs
@@ -1,26 +1,148 @@
-fn main() -> anyhow::Result<()> {
-    // let matches = get_matches();
+use std::{thread, time::Duration};
 
-    // let (incoming_tx, incoming_rx) = mpsc::channel();
-    // let (outgoing_tx, outgoing_rx) = mpsc::channel();
+use anyhow::{anyhow, Result};
+use femtovg::{renderer::OpenGl, Canvas, Color, Paint};
+use glutin::{
+    dpi::PhysicalSize,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+    ContextBuilder,
+};
+pub fn main() -> Result<()> {
+    let window_size = PhysicalSize::new(1000, 700);
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new()
+        .with_inner_size(window_size)
+        .with_resizable(true)
+        .with_title("Modular");
 
-    // let running = Arc::new(AtomicBool::new(true));
-    // let client_address = matches.value_of(CLIENT_ARG).unwrap();
-    // let port = matches.value_of(PORT_ARG).unwrap();
+    let windowed_context = ContextBuilder::new()
+        .with_vsync(true)
+        .build_windowed(wb, &el)?;
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let renderer = OpenGl::new(|s| windowed_context.get_proc_address(s) as *const _)
+        .expect("Cannot create renderer");
+    let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
+    canvas.set_size(
+        window_size.width as u32,
+        window_size.height as u32,
+        windowed_context.window().scale_factor() as f32,
+    );
 
-    // let (_receiving_client_handle, _sending_client_handle) = spawn_client(
-    //     client_address.to_owned(),
-    //     port.to_owned(),
-    //     incoming_tx,
-    //     outgoing_rx,
-    // );
-    // let r = running.clone();
-    // ctrlc::set_handler(move || {
-    //     r.store(false, Ordering::SeqCst);
-    // })
-    // .expect("Error setting Ctrl-C handler");
+    let font = canvas.add_font_mem(include_bytes!(
+        "assets/Fira_Code_v5.2/ttf/FiraCode-Regular.ttf"
+    ))?;
+    println!("{:?}", font);
+    el.run(move |event, window, control_flow| {
+        *control_flow = ControlFlow::Poll;
 
-    // while running.load(Ordering::SeqCst) {}
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { ref event, .. } => match event {
+                WindowEvent::Resized(physical_size) => {
+                    windowed_context.resize(*physical_size);
+                }
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(keycode),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                } => {
+                    if *keycode == VirtualKeyCode::W {
+                        // y -= 0.1;
+                    }
 
-    Ok(())
+                    if *keycode == VirtualKeyCode::S {
+                        // y += 0.1;
+                    }
+
+                    if *keycode == VirtualKeyCode::A {
+                        // x -= 0.1;
+                    }
+
+                    if *keycode == VirtualKeyCode::D {
+                        // x += 0.1;
+                    }
+                }
+                WindowEvent::MouseWheel {
+                    device_id: _,
+                    delta,
+                    ..
+                } => match delta {
+                    glutin::event::MouseScrollDelta::LineDelta(_, y) => {
+                        // font_size = font_size + *y / 2.0;
+                        // font_size = font_size.max(2.0);
+                    }
+                    _ => (),
+                },
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                let dpi_factor = windowed_context.window().scale_factor();
+                let size = windowed_context.window().inner_size();
+                canvas.set_size(size.width as u32, size.height as u32, dpi_factor as f32);
+                canvas.clear_rect(
+                    0,
+                    0,
+                    size.width as u32,
+                    size.height as u32,
+                    Color::rgbf(0.0, 0.0, 0.0),
+                );
+
+                // let elapsed = start.elapsed().as_secs_f32();
+                // let now = Instant::now();
+                // let dt = (now - prevt).as_secs_f32();
+                // prevt = now;
+
+                // perf.update(dt);
+
+                // draw_baselines(&mut canvas, &fonts, 5.0, 50.0, font_size);
+                // draw_alignments(&mut canvas, &fonts, 120.0, 200.0, font_size);
+                // draw_paragraph(&mut canvas, &fonts, x, y, font_size, LOREM_TEXT);
+                // draw_inc_size(&mut canvas, &fonts, 300.0, 10.0);
+
+                // draw_complex(&mut canvas, 300.0, 340.0, font_size);
+
+                // draw_stroked(&mut canvas, &fonts, size.width as f32 - 200.0, 100.0);
+                // draw_gradient_fill(&mut canvas, &fonts, size.width as f32 - 200.0, 180.0);
+                // draw_image_fill(
+                //     &mut canvas,
+                //     &fonts,
+                //     size.width as f32 - 200.0,
+                //     260.0,
+                //     image_id,
+                //     elapsed,
+                // );
+
+                // let mut paint = Paint::color(Color::hex("B7410E"));
+                // paint.set_font(&[fonts.bold]);
+                // paint.set_text_baseline(Baseline::Top);
+                // paint.set_text_align(Align::Right);
+                // let _ = canvas.fill_text(
+                //     size.width as f32 - 10.0,
+                //     10.0,
+                //     format!(
+                //         "Scroll to increase / decrease font size. Current: {}",
+                //         font_size
+                //     ),
+                //     paint,
+                // );
+
+                // canvas.save();
+                // canvas.reset();
+                // perf.render(&mut canvas, 5.0, 5.0);
+                // canvas.restore();
+
+                canvas.flush();
+                windowed_context.swap_buffers().unwrap();
+            }
+            Event::MainEventsCleared => windowed_context.window().request_redraw(),
+            _ => (),
+        }
+    });
 }
