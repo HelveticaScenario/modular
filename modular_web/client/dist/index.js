fd560b7f8e764b59bb39bb6cfff637dae4cda895
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2021-01-25 22:16:24 -0600

    modular web

---
+++ modular_web/client/dist/index.js
@@ -0,0 +1,2 @@
+import r from"react";import e from"react-dom";const o=()=>r.createElement("div",null,"Hello");e.render(r.createElement(o,null),document.getElementById("root"));
+//# sourceMappingURL=index.js.map
