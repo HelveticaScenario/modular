4a33e92f019cc1010f60c8fdae9ed35aaf91d5af
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2021-01-27 21:33:31 -0600

    tailwind

--- modular_web/client/src/app.tsx
+++ modular_web/client/src/app.tsx
@@ -13,6 +13,15 @@ export const App: FunctionComponent<Props> = ({ start }) => {
         }, 1000);
         return () => clearInterval(id);
     }, []);
-
-    return <div class="ring-0">{(start + tick) % 2 === 0 ? 'Hello' : 'Goodbye'}</div>;
+    return (
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div class="flex-shrink-0">
+                <div class="pd ring-4">{(start + tick) % 2 === 0 ? 'Hello' : 'Goodbye'}</div>
+            </div>
+            <div>
+                <div class="text-xl font-medium text-black">ChitChat</div>
+                <p class="text-gray-500">You have a new message!</p>
+            </div>
+        </div>
+    )
 };
--- modular_web/src/main.rs
+++ modular_web/src/main.rs
@@ -5,8 +5,9 @@ async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    let js = warp::path("js").and(warp::fs::dir("./modular_web/client/dist/"));
-    warp::serve(index.or(js).or(hello))
+    let js = warp::path("js").and(warp::fs::dir("./modular_web/client/dist/js"));
+    let css = warp::path("css").and(warp::fs::dir("./modular_web/client/dist/css"));
+    warp::serve(index.or(js).or(css).or(hello))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
