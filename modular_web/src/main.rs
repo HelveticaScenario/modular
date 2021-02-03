c8bef1a50ef097879cee52b2808b2d975b165e55
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2021-02-02 20:42:19 -0600

    make modular_web server compile

--- modular_web/src/main.rs
+++ modular_web/src/main.rs
@@ -1,18 +1,28 @@
 use client::spawn_client;
-use futures::{FutureExt, StreamExt};
+use futures::StreamExt;
+use modular_core::{crossbeam_channel::unbounded, message::InputMessage};
 use modular_server::spawn;
-use modular_core::crossbeam_channel::unbounded;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::{mpsc, RwLock};
-use tokio_stream::wrappers::UnboundedReceiverStream;
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    thread,
+};
+use tokio::sync::{
+    mpsc::{self, UnboundedSender},
+    RwLock,
+};
 use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
 mod client;
 mod osc;
+type Outgoing = Arc<UnboundedSender<InputMessage>>;
+type IncomingMap = Arc<RwLock<HashMap<usize, UnboundedSender<osc::Message>>>>;
 
-type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
-
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 #[tokio::main]
 async fn main() {
     let (_modular_handle, _receiving_server_handle, _sending_server_handle) =
@@ -26,23 +36,94 @@ async fn main() {
         incoming_tx,
         outgoing_rx,
     );
+    let outgoing: Outgoing = {
+        let (tx, mut rx) = mpsc::unbounded_channel();
+        tokio::task::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                if let Err(_) = outgoing_tx.send(msg) {
+                    break;
+                }
+            }
+        });
+        Arc::new(tx)
+    };
+    let mut incoming = {
+        let (tx, rx) = mpsc::unbounded_channel();
+        thread::spawn(move || {
+            while let Ok(msg) = incoming_rx.recv() {
+                if let Err(_) = tx.send(msg) {
+                    break;
+                }
+            }
+        });
+        rx
+    };
 
+    let incoming_map: IncomingMap = Arc::new(RwLock::new(HashMap::new()));
 
-    let (tx, rx) = mpsc::unbounded_channel();
-    let rx = UnboundedReceiverStream::new(rx);
+    let connections = {
+        let incoming_map = incoming_map.clone();
+        let outgoing = outgoing.clone();
+        warp::any().map(move || (outgoing.clone(), incoming_map.clone()))
+    };
 
     let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
     let assets = warp::path("assets").and(warp::fs::dir("./modular_web/client/dist"));
 
-    let ws = warp::path("ws")
-        .and(warp::ws())
-        .map(|ws: warp::ws::Ws| ws.on_upgrade(|websocket| {
+    let ws = warp::path("ws").and(warp::ws()).and(connections).map(
+        |ws: warp::ws::Ws, (outgoing, incoming_map)| {
+            ws.on_upgrade(move |websocket| on_websocket(websocket, incoming_map, outgoing))
+        },
+    );
 
-        }));
-
+    tokio::task::spawn(async move {
+        while let Some(msg) = incoming.recv().await {
+            for sender in incoming_map.read().await.values() {
+                sender.send(msg.clone()).unwrap();
+            }
+        }
+    });
     warp::serve(index.or(assets).or(ws))
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+#[derive(Debug, Clone)]
+enum Command {
+    Incoming(Message),
+    Outgoing(osc::Message),
+}
+
+async fn on_websocket(websocket: WebSocket, incoming_map: IncomingMap, outgoing: Outgoing) {
+    // Use a counter to assign a new unique ID for this user.
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+
+    eprintln!("new user: {}", my_id);
+
+    let (user_ws_tx, mut user_ws_rx) = websocket.split();
+
+    let (tx, mut rx) = mpsc::unbounded_channel();
+    incoming_map.write().await.insert(my_id, tx);
+
+    let (command_tx, mut command_rx) = mpsc::unbounded_channel();
+    
+    let command_tx = Arc::new(command_tx);
+    {
+        let command_tx = command_tx.clone();
+        tokio::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                command_tx.send(Command::Outgoing(msg)).unwrap();
+            }
+        });
+    }
+    tokio::spawn(async move {
+        while let Some(Ok(msg)) = user_ws_rx.next().await {
+            command_tx.send(Command::Incoming(msg)).unwrap();
+        }
+    });
+
+    while let Some(command) = command_rx.recv().await {}
+    incoming_map.write().await.remove(&my_id);
 }
 
 static INDEX_HTML: &str = include_str!("../client/src/index.html");
