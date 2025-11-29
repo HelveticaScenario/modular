26fa0582134499e1bc32764b4630ea7875563504
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2025-11-29 02:44:03 -0500

    remove modular web

--- modular_web/client/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { h, render } from 'deps/preact';
-import { App } from './app';
-
-const root = document.getElementById('root');
-if (!root) {
-    throw new Error('root not found');
-}
-
-render(<App start={0} />, root);
