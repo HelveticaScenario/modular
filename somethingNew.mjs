6572edcc072d15de6aafc283b8c4634142ad6d17
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2025-12-31 00:05:06 -0500

    remove demo files

--- somethingNew.mjs
+++
@@ -1,3 +0,0 @@
-// Simple 440 Hz sine wave
-const osc = sine('osc1').freq(hz(440));
-out.source(osc);
--- test.mjs
+++
@@ -1,6 +0,0 @@
-
-const osc = saw('osc1').shape(sine().freq(hz(1))).freq(note('a4')).scale(2.5);
-scope(osc)
-out.source(osc);
-
-
